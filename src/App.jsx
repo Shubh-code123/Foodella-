f83b018e04d1b81a8cb65f54867113ff65a8f56b
Shubh-code123 <shubhkhatri123@gmail.com>
2025-07-28 17:26:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import OurProduct from "./pages/OurProduct";
 import CrinkleFriesPage from "./pages/CrinkleFriesPage";
