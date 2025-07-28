136c82b969d885aa3e085d894c3d582eff8d12d1
Shubh-code123 <shubhkhatri123@gmail.com>
2025-07-28 17:20:05 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Header from "./Components/Header";
+import Header from "./components/Header";
 import Home from "./Pages/Home";
 import Footer from "./components/Footer";
 import OurProduct from "./pages/OurProduct";
